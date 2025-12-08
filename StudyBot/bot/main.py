74e1240ebb34f2031d7b8cfed95e20abfaf100b9
Mezuna-dev <keith.hounschell@gmail.com>
2025-12-07 23:47:24 -0500

    Add files via upload

--- StudyBot/bot/main.py
+++ StudyBot/bot/main.py
@@ -199,4 +199,3 @@ async def leaderboard(interaction: discord.Interaction):
         msg += f"{idx}. **{entry['discord_name']}** -- {hours}h {minutes}m {seconds}s\n"
     await interaction.response.send_message(msg)
 bot.run(token)
-
